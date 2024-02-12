e26f2460a8a8b50bedb5004d0ef8f067683f8603
Subash Jirel <subashjrl@gmail.com>
2024-02-12 14:40:49 +0545

    added context provider in the layout to pass the cart from shop to the header  using a callback

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+// Layout.jsx
+import React, { createContext, useState, useContext } from 'react';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import { Outlet } from 'react-router-dom';
 
+const CartContext = createContext();
+
 export const Layout = () => {
+  const [cartForHeader, setCartForHeader] = useState([]);
+
+  const handleSetCartForHeader = (dataFromChild) => {
+    setCartForHeader((prev) => [...prev, dataFromChild]);
+    console.log('from the layout');
+    console.log(cartForHeader);
+  };
+
   return (
-    <>
+    <CartContext.Provider value={{ handleSetCartForHeader }}>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </CartContext.Provider>
   );
+};
+
+export const useCartContext = () => {
+  return useContext(CartContext);
 };
--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -1,16 +1,19 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { Card } from '../card/Card';
+import { useCartContext } from '../../Layout';
 
 function Shop() {
   const [data, setData] = useState([]);
-  const [cart, setCart] = useState([]);
+  // const [cart, setCart] = useState([]);
+  const { handleSetCartForHeader } = useCartContext();
 
   function addToCart(itemId) {
     // console.log(itemId);
     const selected = data.find((val) => val.id === itemId);
     // console.log(selected);
-    setCart((prev) => [...prev, selected]);
-    console.log(cart);
+    // setCart((prev) => [...prev, selected]);
+    handleSetCartForHeader(selected);
+    // console.log(cart);
   }
   useEffect(() => {
     fetch('https://fakestoreapi.com/products')
@@ -22,26 +25,33 @@ function Shop() {
       .catch((error) => console.error(error));
   }, []);
 
+  //   useEffect(()=>{
+  //     function getDefaultCart() {
+  //         let inTheRightCart = {}
+  //         for(let i=1; i<data.length; i++) {
+  //             inTheRightCart[i]=0
+  //         }
+  //       }
+  //   },[data])
+
   return (
-    <>
-      <div className="h-full min-h-screen ">
-        <ul className="grid autoFitThisGrid mt-4">
-          {data.map((item) => (
-            <li key={item.id} className="border ">
-              <Card
-                itemId={item.id}
-                price={item.price}
-                title={item.title.split(' ').slice(0, 3).join(' ')}
-                image={item.image}
-                description={item.description}
-                addToCart={addToCart}
-              />
-            </li>
-            //   <li key={index}>{item.title}</li>
-          ))}
-        </ul>
-      </div>
-    </>
+    <div className="h-full min-h-screen ">
+      <ul className="grid autoFitThisGrid mt-4">
+        {data.map((item) => (
+          <li key={item.id} className="border ">
+            <Card
+              itemId={item.id}
+              price={item.price}
+              title={item.title.split(' ').slice(0, 3).join(' ')}
+              image={item.image}
+              description={item.description}
+              addToCart={addToCart}
+            />
+          </li>
+          //   <li key={index}>{item.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 export default Shop;
